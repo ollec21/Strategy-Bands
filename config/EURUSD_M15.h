567a49076f96201d9bdcfd079763c00cb4589f5a
kenorb <kenorb@users.noreply.github.com>
2021-01-01 19:05:09 +0000

    Updates input params

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -7,9 +7,9 @@
 struct Indi_Bands_Params_M15 : Indi_Bands_Params {
   Indi_Bands_Params_M15() : Indi_Bands_Params(indi_bands_defaults, PERIOD_M15) {
     applied_price = (ENUM_APPLIED_PRICE)0;
-    bshift = 0;
+    bshift = 2;
     deviation = 1.7;
-    period = 22;
+    period = 16;
     shift = 0;
   }
 } indi_bands_m15;
