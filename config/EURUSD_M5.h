2b3761afeb544ebbd73df2af907003d04114a82d
kenorb <kenorb@users.noreply.github.com>
2021-01-10 20:13:34 +0000

    config: Updates deviation values

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -8,7 +8,7 @@ struct Indi_Bands_Params_M5 : Indi_Bands_Params {
   Indi_Bands_Params_M5() : Indi_Bands_Params(indi_bands_defaults, PERIOD_M5) {
     applied_price = (ENUM_APPLIED_PRICE)0;
     bshift = 5;
-    deviation = 0.5;
+    deviation = 0.6;
     period = 16;
     shift = 0;
   }
